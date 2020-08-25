bc4f7f89348b63f34f81cda96481a85a0c26e6c3
Kengwang <1136772134@qq.com>
2020-08-25 10:03:07 +0800

    Fixed Native Error

--- EasyCraft/Web/Api.cs
+++ EasyCraft/Web/Api.cs
@@ -10,6 +10,13 @@ namespace EasyCraft.Web
 {
     class Api
     {
+        public static System.Text.Json.JsonSerializerOptions option = new System.Text.Json.JsonSerializerOptions();
+
+        public static void init()
+        {
+            option.IgnoreNullValues = true;
+        }
+
         public static void PhraseAPI(string path, WebPanelPhraser wp)
         {
             switch (path)
@@ -21,7 +28,7 @@ namespace EasyCraft.Web
                         wp.session["loginfail"] += 1;
                         callback.message = Language.t("Login failed, too many attempts!");
                         callback.code = -2;
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         return;
                     }
                     if (wp.POST.ContainsKey("username") && wp.POST.ContainsKey("password"))
@@ -37,7 +44,7 @@ namespace EasyCraft.Web
                             callback.data.username = wp.vars.user.name;
                             callback.data.uid = wp.vars.user.uid;
                             callback.data.email = wp.vars.user.email;
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
                         else
                         {
@@ -54,7 +61,7 @@ namespace EasyCraft.Web
                             wp.session["lastloginfailday"] = DateTime.Today.ToString();
                             callback.message = Language.t("Login failed, account or password is wrong");
                             callback.code = -1;
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
                     }
                     else
@@ -62,7 +69,7 @@ namespace EasyCraft.Web
                         UserLogin callback = new UserLogin();
                         callback.message = Language.t("Login failed, parameters are incomplete!");
                         callback.code = -3;
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(Serialize(callback, option));
                     }
                     break;
                 case "register":
@@ -73,7 +80,7 @@ namespace EasyCraft.Web
                             Register callback = new Register();
                             callback.code = -2;
                             callback.message = Language.t("Registration failed, username already exists");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
                         else
                         {
@@ -83,14 +90,14 @@ namespace EasyCraft.Web
                                 Register callback = new Register();
                                 callback.code = -2;
                                 callback.message = Language.t("Registration failed");
-                                wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                                wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                             }
                             else
                             {
                                 Register callback = new Register();
                                 callback.code = 9000;
                                 callback.message = Language.t("Registration Success");
-                                wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                                wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                             }
                         }
                     }
@@ -99,7 +106,7 @@ namespace EasyCraft.Web
                         Register callback = new Register();
                         callback.code = -3;
                         callback.message = Language.t("Registration failed with incomplete parameters");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     break;
                 case "new_server":
@@ -128,14 +135,14 @@ namespace EasyCraft.Web
                                 callback.code = 9000;
                                 callback.message = Language.t("Server Successfully Create");
                                 callback.data = s.id;
-                                wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                                wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                             }
                             catch (Exception)
                             {
                                 NewServer callback = new NewServer();
                                 callback.code = -1;
                                 callback.message = Language.t("Failed to Create Server");
-                                wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                                wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                             }
 
                         }
@@ -144,7 +151,7 @@ namespace EasyCraft.Web
                             NewServer callback = new NewServer();
                             callback.code = -2;
                             callback.message = Language.t("Param not completed");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
                     }
                     else
@@ -152,7 +159,7 @@ namespace EasyCraft.Web
                         NewServer callback = new NewServer();
                         callback.code = -3;
                         callback.message = Language.t("Permission Denied");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     break;
                 case "edit_server":
@@ -161,7 +168,7 @@ namespace EasyCraft.Web
                         Callback callback = new Callback();
                         callback.code = -1;
                         callback.message = Language.t("Server Not Found");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         return;
                     }
                     Server server = ServerManager.servers[int.Parse(wp.POST["sid"])];
@@ -194,7 +201,7 @@ namespace EasyCraft.Web
                             Callback callback = new Callback();
                             callback.code = 9000;
                             callback.message = Language.t("Server Edited");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                             return;
                         }
                         catch (Exception)
@@ -202,7 +209,7 @@ namespace EasyCraft.Web
                             NewServer callback = new NewServer();
                             callback.code = -2;
                             callback.message = Language.t("Edit Server Config Failed");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
 
                     }
@@ -211,7 +218,7 @@ namespace EasyCraft.Web
                         NewServer callback = new NewServer();
                         callback.code = -3;
                         callback.message = Language.t("Permission Denied");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     break;
                 case "start_server":
@@ -220,7 +227,7 @@ namespace EasyCraft.Web
                         Callback callback = new Callback();
                         callback.code = -1;
                         callback.message = Language.t("Server Not Found");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         return;
                     }
                     if (wp.vars.user.type >= 2 || ServerManager.servers[int.Parse(wp.POST["sid"])].owner == wp.vars.user.uid)
@@ -230,7 +237,7 @@ namespace EasyCraft.Web
                             Callback callback = new Callback();
                             callback.code = -3;
                             callback.message = Language.t("Server Expired");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                             return;
                         }
                         try
@@ -239,7 +246,7 @@ namespace EasyCraft.Web
                             Callback callback = new Callback();
                             callback.code = 9000;
                             callback.message = Language.t("Server Started");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                             return;
                         }
                         catch (Exception)
@@ -247,7 +254,7 @@ namespace EasyCraft.Web
                             Callback callback = new Callback();
                             callback.code = -2;
                             callback.message = Language.t("Failed to Start Server");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
                     }
                     else
@@ -255,7 +262,7 @@ namespace EasyCraft.Web
                         NewServer callback = new NewServer();
                         callback.code = -3;
                         callback.message = Language.t("Permission Denied");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     break;
                 case "stop_server":
@@ -264,7 +271,7 @@ namespace EasyCraft.Web
                         Callback callback = new Callback();
                         callback.code = -1;
                         callback.message = Language.t("Server Not Found");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         return;
                     }
                     if (wp.vars.user.type >= 2 || ServerManager.servers[int.Parse(wp.POST["sid"])].owner == wp.vars.user.uid)
@@ -275,7 +282,7 @@ namespace EasyCraft.Web
                             Callback callback = new Callback();
                             callback.code = 9000;
                             callback.message = Language.t("Server Stopped");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                             return;
                         }
                         catch (Exception)
@@ -283,7 +290,7 @@ namespace EasyCraft.Web
                             Callback callback = new Callback();
                             callback.code = -2;
                             callback.message = Language.t("Failed to Stop Server");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
                     }
                     else
@@ -291,7 +298,7 @@ namespace EasyCraft.Web
                         NewServer callback = new NewServer();
                         callback.code = -3;
                         callback.message = Language.t("Permission Denied");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     break;
                 case "log":
@@ -300,7 +307,7 @@ namespace EasyCraft.Web
                         Callback callback = new Callback();
                         callback.code = -1;
                         callback.message = Language.t("Server Not Found");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         return;
                     }
                     if (wp.vars.user.type >= 2 || ServerManager.servers[int.Parse(wp.POST["sid"])].owner == wp.vars.user.uid)
@@ -336,14 +343,14 @@ namespace EasyCraft.Web
                                 callback.data.lastlogid = 0;
                             }
                         }
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     else
                     {
                         Callback callback = new Callback();
                         callback.code = -3;
                         callback.message = Language.t("Permission Denied");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     break;
                 case "cmd":
@@ -352,7 +359,7 @@ namespace EasyCraft.Web
                         Callback callback = new Callback();
                         callback.code = -1;
                         callback.message = Language.t("Server Not Found");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         return;
                     }
                     if (wp.vars.user.type >= 2 || ServerManager.servers[int.Parse(wp.POST["sid"])].owner == wp.vars.user.uid)
@@ -363,14 +370,14 @@ namespace EasyCraft.Web
                             Callback callback = new Callback();
                             callback.code = 9000;
                             callback.message = Language.t("Success");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
                         else
                         {
                             Callback callback = new Callback();
                             callback.code = -2;
                             callback.message = Language.t("Param not completed");
-                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                            wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         }
                     }
                     else
@@ -378,7 +385,7 @@ namespace EasyCraft.Web
                         Callback callback = new Callback();
                         callback.code = -3;
                         callback.message = Language.t("Permission Denied");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     break;
                 case "clear_log":
@@ -387,7 +394,7 @@ namespace EasyCraft.Web
                         Callback callback = new Callback();
                         callback.code = -1;
                         callback.message = Language.t("Server Not Found");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                         return;
                     }
                     if (wp.vars.user.type >= 2 || ServerManager.servers[int.Parse(wp.POST["sid"])].owner == wp.vars.user.uid)
@@ -396,14 +403,14 @@ namespace EasyCraft.Web
                         Callback callback = new Callback();
                         callback.code = 9000;
                         callback.message = Language.t("Successfully to clear logs on server");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     else
                     {
                         Callback callback = new Callback();
                         callback.code = -3;
                         callback.message = Language.t("Permission Denied");
-                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback));
+                        wp.PrintWeb(System.Text.Json.JsonSerializer.Serialize(callback, option));
                     }
                     break;
             }
