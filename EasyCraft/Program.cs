6523808784012cdad6fa03408e805e977645c866
kengwang <atkengwang@qq.com>
2021-08-08 22:33:10 +0800

    Initial Commit

---
+++ EasyCraft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyCraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello EasyCraft!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# EasyCraft
+
+下一代 Minecraft 服务器面板
+
+The Next Generation Minecraft Server Panel
+
+项目开发中......
+
+如要加入内测请联系加入QQ群: 965085376
+
